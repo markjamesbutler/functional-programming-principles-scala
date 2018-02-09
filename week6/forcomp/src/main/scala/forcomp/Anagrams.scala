77451fdfb2b58efdfbd3fb3d2021fabeeeb6e3e3
markjamesbutler <mark.butler@guardian.co.uk>
2018-02-09 21:23:20 +0000

    Dictionary by occurances.

--- week6/forcomp/src/main/scala/forcomp/Anagrams.scala
+++ week6/forcomp/src/main/scala/forcomp/Anagrams.scala
@@ -1,6 +1,6 @@
 package forcomp
 
-import forcomp.Anagrams.Occurrences
+import forcomp.Anagrams.{Occurrences, Word, dictionary, wordOccurrences}
 
 
 object Anagrams {
@@ -63,7 +63,7 @@ object Anagrams {
    *    List(('a', 1), ('e', 1), ('t', 1)) -> Seq("ate", "eat", "tea")
    *
    */
-  lazy val dictionaryByOccurrences: Map[Occurrences, List[Word]] = ???
+  lazy val dictionaryByOccurrences: Map[Occurrences, List[Word]] = dictionary.groupBy(wordOccurrences(_))
 
   /** Returns all the anagrams of a given word. */
   def wordAnagrams(word: Word): List[Word] = ???
