84b00641327f4f92b1b2aba25df6c30442430a13
markjamesbutler <mark.butler@guardian.co.uk>
2018-02-10 10:57:40 +0000

    combinations empty.

--- week6/forcomp/src/main/scala/forcomp/Anagrams.scala
+++ week6/forcomp/src/main/scala/forcomp/Anagrams.scala
@@ -94,7 +94,18 @@ object Anagrams {
    *  Note that the order of the occurrence list subsets does not matter -- the subsets
    *  in the example above could have been displayed in some other order.
    */
-  def combinations(occurrences: Occurrences): List[Occurrences] = ???
+  def combinations(occurrences: Occurrences): List[Occurrences] = {
+
+    if (occurrences.isEmpty) List(Nil)
+    else {
+
+      val o = for {
+        occurrence <- occurrences
+      } yield occurrence
+
+    }
+    List(occurrences)
+  }
 
   /** Subtracts occurrence list `y` from occurrence list `x`.
    *
